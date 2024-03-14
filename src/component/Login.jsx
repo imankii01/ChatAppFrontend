2978867ce8eda99f4dadc8f1714079a121872032
Ankit <codewithankit047@gmail.com>
2024-03-14 14:53:04 +0530

    updates

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
-import { Form, Input, Button, notification } from "antd";
+import React, { useState, useEffect } from "react";
+import { Form, Input, Button, notification, message } from "antd";
 import axios from "axios";
+import { motion } from "framer-motion";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [showOtpInput, setShowOtpInput] = useState(false);
   const [otp, setOtp] = useState("");
   const [loadingOtp, setLoadingOtp] = useState(false);
-  const [name, setName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
   const [loadingSave, setLoadingSave] = useState(false);
   const [currentStep, setCurrentStep] = useState(1);
+  const [timeRemaining, setTimeRemaining] = useState(60); // Initial time remaining until next OTP can be sent
+
+  useEffect(() => {
+    let timer;
+    if (timeRemaining > 0) {
+      timer = setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+    }
+    return () => clearTimeout(timer);
+  }, [timeRemaining]);
 
   const handleEmailSubmit = async () => {
     try {
@@ -22,6 +32,7 @@ const Login = () => {
       if (response.data.success) {
         setShowOtpInput(true);
         setCurrentStep(2);
+        setTimeRemaining(60); // Reset time remaining when OTP is sent
         notification.success({
           message: "OTP Sent",
           description: "An OTP has been sent to your email address.",
@@ -61,6 +72,9 @@ const Login = () => {
         });
         setShowOtpInput(false);
         setCurrentStep(3);
+        // Navigate to the add-up-details screen
+        // navigate("AddUpDetails");
+        message.success("navigation start")
       } else {
         notification.error({
           message: "Invalid OTP",
@@ -78,177 +92,101 @@ const Login = () => {
     }
   };
 
-  const handleNameSubmit = async () => {
-    try {
-      setLoadingSave(true);
-      const response = await axios.post(
-        "http://localhost:3001/api/save-user-details",
-        {
-          email,
-          name,
-          phoneNumber,
-        }
-      );
-
-      if (response.data.success) {
-        notification.success({
-          message: "User Details Saved",
-          description: "Your details have been successfully saved.",
-        });
-        // You can redirect the user to the next page or perform additional actions here
-        // For now, let's redirect to the dashboard
-        // history.push('/dashboard');
-        alert("done");
-      } else {
-        notification.error({
-          message: "Error",
-          description: "Failed to save user details. Please try again.",
-        });
-      }
-    } catch (error) {
-      console.error("Error saving user details:", error);
-      notification.error({
-        message: "Error",
-        description: "An error occurred while processing your request.",
+  const handleSendOtpAgain = () => {
+    if (timeRemaining === 0) {
+      handleEmailSubmit();
+    } else {
+      notification.warning({
+        message: "Please wait",
+        description: `You can send OTP again in ${timeRemaining} seconds.`,
       });
-    } finally {
-      setLoadingSave(false);
     }
   };
 
   return (
-    <section className="vh-100 gradient-custom">
-      <div className="container py-5 h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div
-              className="card bg-dark text-white"
-              style={{ borderRadius: "1rem" }}
-            >
-              <div className="card-body p-5 text-center">
-                <div className="mb-md-5 mt-md-4 pb-5">
-                  <h2 className="fw-bold mb-2 text-uppercase">
-                    GupChup -Chat app
-                  </h2>
-                  <p className="text-white-50 mb-5">
-                    A highly secure chat webApp.
-                  </p>
-                  {currentStep === 1 && (
-                    <Form onFinish={handleEmailSubmit}>
-                      <Form.Item
-                        name="text"
-                        rules={[
-                          {
-                            required: true,
-                            message: "Please enter your email!",
-                          },
-                          { type: "email", message: "Invalid email format!" },
-                        ]}
-                      >
-                        <Input
-                          type="text" // Change the type from 'email' to 'text'
-                          className="form-control form-control-lg"
-                          placeholder="Email"
-                          onChange={(e) => setEmail(e.target.value)}
-                        />
-                      </Form.Item>
+    <motion.section
+      className="gradient-custom"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="card bg-dark text-white" style={{ borderRadius: "1rem" }}>
+        <div className="card-body p-5 text-center">
+          <div className="mb-md-5 mt-md-4 pb-5">
+            <h2 className="fw-bold mb-2 text-uppercase">GupChup -Chat app</h2>
+            <p className="text-white-50 mb-5">A highly secure chat webApp.</p>
+            {currentStep === 1 && (
+              <Form onFinish={handleEmailSubmit}>
+                <Form.Item
+                  name="email"
+                  rules={[
+                    { required: true, message: "Please enter your email!" },
+                    { type: "email", message: "Invalid email format!" },
+                  ]}
+                >
+                  <Input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Form.Item>
 
-                      <Form.Item>
-                        <Button
-                          className="btn btn-outline-light btn-lg px-5"
-                          type="submit"
-                          loading={loadingOtp}
-                          onClick={handleEmailSubmit}
-                        >
-                          Send OTP
-                        </Button>
-                      </Form.Item>
-                    </Form>
-                  )}
-                  {currentStep === 2 && (
-                    <Form onFinish={handleOtpSubmit}>
-                      <Form.Item
-                        name="otp"
-                        rules={[
-                          {
-                            required: true,
-                            message: "Please enter the OTP sent to your email!",
-                          },
-                        ]}
-                      >
-                        <Input
-                          type="text"
-                          id="typeOtp"
-                          className="form-control form-control-lg"
-                          placeholder="Enter OTP"
-                          onChange={(e) => setOtp(e.target.value)}
-                        />
-                      </Form.Item>
-                      <Form.Item>
-                        <Button
-                          className="btn btn-outline-light btn-lg px-5"
-                          type="submit"
-                          loading={loadingSave}
-                          onClick={handleOtpSubmit}
-                        >
-                          Verify OTP
-                        </Button>
-                      </Form.Item>
-                    </Form>
-                  )}
-                  {currentStep === 3 && (
-                    <Form onFinish={handleNameSubmit}>
-                      <Form.Item
-                        name="name"
-                        rules={[
-                          {
-                            required: true,
-                            message: "Please enter your name!",
-                          },
-                        ]}
-                      >
-                        <Input
-                          id="name"
-                          className="form-control form-control-lg"
-                          placeholder="Name"
-                          onChange={(e) => setName(e.target.value)}
-                        />
-                      </Form.Item>
-                      <Form.Item
-                        name="phoneNumber"
-                        rules={[
-                          {
-                            required: true,
-                            message: "Please enter your phone number!",
-                          },
-                        ]}
-                      >
-                        <Input
-                          id="phoneNumber"
-                          className="form-control form-control-lg"
-                          placeholder="Phone Number"
-                          onChange={(e) => setPhoneNumber(e.target.value)}
-                        />
-                      </Form.Item>
-                      <Form.Item>
-                        <Button
-                          className="btn btn-outline-light btn-lg px-5"
-                          type="submit"
-                          loading={loadingSave}
-                          onClick={handleNameSubmit}
-                        >
-                          Save Details
-                        </Button>
-                      </Form.Item>
-                    </Form>
-                  )}
-                </div>
-              </div>
-            </div>
+                <Form.Item>
+                  <Button
+                    className="btn btn-outline-light btn-lg px-5"
+                    type="submit"
+                    loading={loadingOtp}
+                    onClick={handleEmailSubmit}
+                  >
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            )}
+            {currentStep === 2 && (
+              <Form onFinish={handleOtpSubmit}>
+                <Form.Item
+                  name="otp"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter the OTP sent to your email!",
+                    },
+                  ]}
+                >
+                  <Input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Enter OTP"
+                    onChange={(e) => setOtp(e.target.value)}
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    className="btn btn-outline-light btn-lg px-5"
+                    type="submit"
+                    loading={loadingSave}
+                    onClick={handleOtpSubmit}
+                  >
+                    Verify OTP
+                  </Button>
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    className="btn btn-outline-light btn-lg px-5"
+                    onClick={handleSendOtpAgain}
+                    disabled={timeRemaining !== 0}
+                  >
+                    Send OTP Again ({timeRemaining}s)
+                  </Button>
+                </Form.Item>
+              </Form>
+            )}
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
